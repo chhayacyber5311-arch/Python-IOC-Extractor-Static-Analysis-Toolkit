2cac28f9d7767afe34963c94320595ed1b8c282f
Chhaya Chaudhari <chhaya.cyber5311@gmail.com>
2026-07-03 20:17:07 +0530

    Add CLI validation and extractor module skeleton

--- extractor.py
+++ extractor.py
@@ -1,3 +1,18 @@
+#  Impporting re for regex patterns, means reading text and find the given pattern in file
+import re
+
+# Impoeting IP_PATTERN from patterns.py
+from patterns import IP_PATTERN
+
+# ips = re.findall -> to find givem pattern in IP_PATTERN the format in target text file 
+# set -> remove duplicate ips in file but can change order
+# dict.fromkeys -> will remove duplicate but keep first seen order
+# list -> again list all ips found in file
+
 def extract_iocs(content):
     print("[+] Extractor received file content.")
-    return{}
+    ips = list(dict.fromkeys(re.findall(IP_PATTERN, content)))
+
+    return{
+        "IPs": ips
+    }
