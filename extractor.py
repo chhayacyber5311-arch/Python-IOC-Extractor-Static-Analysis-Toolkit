3d81be149b391f7c8bb1b9e0ac435ccd5efa9aa0
Chhaya Chaudhari <chhaya.cyber5311@gmail.com>
2026-07-06 13:03:14 +0530

    Add IPv4 extraction and validation

--- extractor.py
+++ extractor.py
@@ -1,8 +1,11 @@
 #  Impporting re for regex patterns, means reading text and find the given pattern in file
 import re
 
-# Impoeting IP_PATTERN from patterns.py
+# Importing IP_PATTERN from patterns.py
 from patterns import IP_PATTERN
+
+# Importing from utils.py, is_valid_ipv4
+from utils import is_valid_ipv4
 
 # ips = re.findall -> to find givem pattern in IP_PATTERN the format in target text file 
 # set -> remove duplicate ips in file but can change order
@@ -11,7 +14,10 @@ from patterns import IP_PATTERN
 
 def extract_iocs(content):
     print("[+] Extractor received file content.")
-    ips = list(dict.fromkeys(re.findall(IP_PATTERN, content)))
+    raw_ips = re.findall(IP_PATTERN, content)
+    inorder_ips = list(dict.fromkeys(raw_ips))
+    ips = [ip for ip in inorder_ips if is_valid_ipv4(ip)]
+
 
     return{
         "IPs": ips
